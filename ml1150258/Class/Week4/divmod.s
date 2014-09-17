edcfc2bac3e2e2a373103c801c3e7b7f51eb89d3
ml1150258 <mark@omniplatform.com>
2014-09-16 18:01:55 -0700

    Signed-off-by: ml1150258 <mark@omniplatform.com>

--- ml1150258/Class/Week4/divmod.s
+++ ml1150258/Class/Week4/divmod.s
@@ -14,10 +14,10 @@ main:
     mov r2, #111     /* a=111 */
     mov r3, #5       /* b=5 */
 	mov r0, #0       /* r0=0 -> counter */
-	mov r1, #111       /* r1=a=111 */
+	mov r1, r2       /* r1=a=111 */
 repeat_subtraction:
-	sub r1, r1, r2   /* r1-r1-r2 */
-	cmp r1, r2        
+	sub r1, r1, r3   /* r1=r1-r2 */
+	cmp r1, r3        
     blt end          /* end the program solution reached */
 	add r0, r0, #1   /* increment r0 */
 	b repeat_subtraction  /* repeat the subtraction */
