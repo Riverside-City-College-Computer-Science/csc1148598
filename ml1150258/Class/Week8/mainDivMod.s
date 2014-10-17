226e995e73f7fbf4db3f3d5dc70614ebd801cf06
ml1150258 <mark@omniplatform.com>
2014-10-16 20:59:31 -0700

    Signed-off-by: ml1150258 <mark@omniplatform.com>

--- ml1150258/Class/Week8/mainDivMod.s
+++ ml1150258/Class/Week8/mainDivMod.s
@@ -58,6 +58,7 @@ main:
 	add r1, sp, #4               /* and second value as a of scanf */
     ldr r1, [r1]                 /* Load the integer a read by scanf into r1 */
     ldr r2, [sp]		         /* Load the integer b read by scanf into r2 */
+	bl scaleRight
     ldr r0, address_of_message3  /* Set &message2 as the first parameter of printf */
     bl printf                    /* Call printf */
  
